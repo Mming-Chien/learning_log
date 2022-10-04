1d658d1e151a6b852c4b7737835610be25129ac3
Mming-Chien <113403394+Mming-Chien@users.noreply.github.com>
2022-10-04 16:25:41 +0700

    login required and connecting data to certain user
    Restricting access with login required and connecting data to certain users so each user have their own data

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -9,12 +10,14 @@ def index(request):
 	""" The home page for learning log"""
 	return render(request, "learning_logs/index.html")
 
+@login_required
 def topics(request):
 	"""Show all topics"""
 	topics = Topic.objects.order_by("date_added")
 	context = {"topics": topics}
 	return render(request, "learning_logs/topics.html", context)
 
+@login_required
 def topic(request, topic_id):
 	"""Show a single topic and all its entries."""
 	topic = Topic.objects.get(id=topic_id)
@@ -22,6 +25,7 @@ def topic(request, topic_id):
 	context = {"topic":topic, "entries":entries}
 	return render(request, "learning_logs/topic.html", context)
 
+@login_required
 def new_topic(request):
 	""" Adding new toppic"""
 	if request.method != "POST":
@@ -38,6 +42,7 @@ def new_topic(request):
 	context = {"form": form}
 	return render(request, "learning_logs/new_topic.html", context)
 
+@login_required
 def new_entry(request, topic_id):
 	""" Adding new entry for particular topic"""
 	topic = Topic.objects.get(id=topic_id)
@@ -57,6 +62,7 @@ def new_entry(request, topic_id):
 	context = {"topic": topic, "form": form}
 	return render(request, "learning_logs/new_entry.html", context)
 
+@login_required
 def edit_entry(request, entry_id):
 	"""Editing an existing entry"""
 	entry = Entry.objects.get(id=entry_id)
