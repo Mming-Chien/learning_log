a6511bacef4494e865efb012ae8db18358890d63
Mming-Chien <113403394+Mming-Chien@users.noreply.github.com>
2022-10-07 16:20:20 +0700

    Style topics and entries pages

--- learning_logs/views.py
+++ learning_logs/views.py
@@ -86,6 +86,9 @@ def edit_entry(request, entry_id):
 		if form.is_valid():
 			form.save()
 			return redirect("learning_logs:topic", topic_id=topic.id)
+	# Display a form for editing
+	context = {"entries": entries, "form":form, "topic": topic}
+	return render(request, "learning_logs/edit_entry.html", context)
 
 def check_topic_owner(topic,request):
 	"""Check whether the owner of the topic matches currently logged in user"""
@@ -93,5 +96,5 @@ def check_topic_owner(topic,request):
 		raise Http404
 
 	# Display a form for editing
-	context = {"entry": entry, "form":form, "topic": topic}
-	return render(request, "learning_logs/edit_entry.html", context)
+	#context = {"entries": entries, "form":form, "topic": topic}
+	#return render(request, "learning_logs/edit_entry.html", context)
